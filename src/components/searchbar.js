038ad027a8507c8dce3c4dd7763369659aa2af99
cartercw <wccarter58@gmail.com>
2018-10-17 11:47:49 -0400

    updated react

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -6,6 +6,12 @@ import React from 'react';
 // class SearchBar extends Component {
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {term: ''};
+        
+    }
     // create a class called searchbar and extend it /give it access to everything that a React Component can do or have and functionality
     render () {
         // onChange is referencing the event that is goinng to take place which is when the client changes what they are submitting in the input box for our application
@@ -16,18 +22,27 @@ class SearchBar extends React.Component {
     // every class component will have a render method for components we have to have the curley braces with them
 
     onInputChange(event) {
+        this.setState({
+            term: event.target.value
+        })
+        // console.log(event.target.value);
         // event, e, eventObject all reference the actual event takig place
         // to get access to the value of that event we need to look at event.target.value - target on the event itself is referrencing the input vlaue aka the text field and the value is the text and words being typed.
-        console.log(event.target.value);
     }
 }
+export default SearchBar;
 // creating an event orwatching an event from the client is going to be handled by an event handler
 
 // theres are 2 steps to creating an event handler
 
 // 1. declare an event handler which is a function that should be ran whenever the event occurs
 // 2. we pass the event handler to the element we want to monitor the events.  
-export default SearchBar;
 
 //**** in ES6 we can actually get rid of the onInputChange function if we want and instead can call 
 // <input onChange={event => console.log(event.target.value)} this is still valid and is ES6 and works in the input tag just like the current es5 function is being called in the input tag
+
+// ***STATE IS A PLAIN JAVASCRIPT OBJECT THAT IS CREATED TO RECORD AND REACT TO USER EVENTS!
+// this means based on things that are happening stat can be updated and changed 
+
+// *** EACH CLASS BASED COMPONENT HAS ITS OWN  STATE OBJECT - WHENEVER A COMPONENTS STATE IS CHANGED IT IMMEDIATELY RE-RENDERS AND FORCES ALL OF ITS CHILDREN TO RE-RENDER AS WELL
+// FUNCTIONAL COMPONENTS DO NOT HAVE STATE!
